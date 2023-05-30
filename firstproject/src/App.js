a19145bb515b564516f7ac19a11a341e643fb923
eaglehero <kingeagle2040@gmail.com>
2023-05-30 23:34:52 +0300

    first commit

---
+++ firstproject/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import First from "./Components/First";
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <First />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ firstproject/src/Components/First.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function First(){
+    return (
+        <div>
+            <h2>welcome mina to react first project</h2>
+            <h2>nice to see you and your good team work in the first project</h2>
+            <h3>welcome that is the better new step to atcive your dream</h3>
+        </div>
+    )
+}
+
+export default First;

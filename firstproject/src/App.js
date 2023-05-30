964015a8eff46360100c3bf40447bd9b72056535
eaglehero <kingeagle2040@gmail.com>
2023-05-30 23:47:18 +0300

    edite app.js file

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
 import First from "./Components/First";
 import Second from "./Components/Second";
+import Third from "./Components/third";
 class App extends Component {
   render(){
     return (
       <div className="App">
         <First />
         <Second />
+        <Third />
       </div>
     )
   }
---
+++ firstproject/src/Components/Third.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+function Third(){
+    return(
+        <div>
+            <h2>welcome Mr.Mina with new family and new company</h2>
+        </div>
+    )
+}
+export default Third;
